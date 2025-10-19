6b2771a50ea60e819d5377c302e8e413eda847f7
Zaid Ilyas <81157594+zaidilyas89@users.noreply.github.com>
2025-10-19 13:54:06 +0500

    modified import langchain

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import google.generativeai as genai
 from pypdf import PdfReader
 import faiss
 import numpy as np
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 import os
 from fpdf import FPDF
 import io
